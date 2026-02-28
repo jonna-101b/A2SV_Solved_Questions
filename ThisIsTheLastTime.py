033e00fca50ff8e8add84249acb82db3a43e20d8
Jonna <151217814+jonna-101b@users.noreply.github.com>
2026-02-28 16:35:32 +0300

    Create ThisIsTheLastTime.py

---
+++ ThisIsTheLastTime.py
@@ -0,0 +1,18 @@
+if __name__ == "__main__":
+    no_of_testcases = int(input())
+
+    for _ in range(no_of_testcases):
+        n, coins = [ int(num) for num in input().strip().split() ]
+        casinos = []
+        
+        for __ in range(n):
+            l, r, real = [ int(num) for num in input().strip().split() ]
+            casinos.append((l, real, r))
+
+        casinos.sort(key = lambda x: x[0])
+        
+        for l, r, real in casinos:
+            if l <= coins <= r:
+                coins = max(coins, real)
+
+        print(coins)
