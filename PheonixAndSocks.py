5bf8dfe3874d1df6167813a3f10bfd52a73cb46c
Jonna <151217814+jonna-101b@users.noreply.github.com>
2026-03-02 15:05:05 +0000

    Subtle change

--- PheonixAndSocks.py
+++ PheonixAndSocks.py
@@ -1,10 +1,20 @@
+from collections import Counter
+
 if __name__ == "__main__":
     no_of_testcases = int(input())
 
     for _ in range(no_of_testcases):
         n, l, r = [ int(num) for num in input().strip().split() ]
         socks = [ int(num) for num in input().strip().split() ]
-        left = set(socks[:n//2])
-        right = set(socks[n//2:])
-        costs = abs(n//2 - l) + len(left - right)
+        left = socks[:n//2]
+        right = Counter(socks[n//2:])
+        costs = abs(n//2 - l)
+
+        for num in left:
+            if num not in right or right[num] == 0:
+                costs += 1
+                continue
+
+            right[num] -= 1
+            
         print(costs)
