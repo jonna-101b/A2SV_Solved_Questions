8dbc4ce1b28d19df89dcb8851718cc3e42512278
Jonna <151217814+jonna-101b@users.noreply.github.com>
2026-04-08 20:54:54 +0300

    Implement N-Queens II solution using DFS

---
+++ NQueensII.py
@@ -0,0 +1,27 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        dx=set()
+        dy=set()
+        cols=set()
+        ans=[["."]*n for _ in range(n)]
+        res=[]
+
+        def dfs(i):
+            if i==n:
+                res.append(["".join(k) for k in ans])
+                return
+
+            for j in range(n):
+                if i-j in dx or i+j in dy or j in cols:
+                    continue
+                ans[i][j]="Q"
+                dx.add(i-j)
+                dy.add(i+j)
+                cols.add(j)
+                dfs(i+1)
+                ans[i][j]="."
+                dx.remove(i-j)
+                dy.remove(i+j)
+                cols.remove(j)
+        dfs(0)
+        return len(res)
