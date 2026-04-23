e4dbd9da32adeebcff3c1b40be4605b0e528fb71
Jonna <151217814+jonna-101b@users.noreply.github.com>
2026-04-23 13:01:53 +0000

    Subtle change

--- MashaAndABeautifulTree.py
+++ MashaAndABeautifulTree.py
@@ -1,35 +1,42 @@
 from typing import Optional, List
 from math import log2
 
-if __name__ == "__main__":
-    no_of_testcases = int(input())
+def solve(n: int, p: List[int]) -> int:
+    index = -1
+    ops = 0
 
-    for _ in range(no_of_testcases):
+    def DFS(counter: int = 0) -> Optional[List[int]]:
+        nonlocal index, ops
+
+        if ops == -1:
+            return None
+
+        if counter == n:
+            index += 1
+            return [p[index], p[index]]
+        
+        r1 = DFS(counter + 1)
+        r2 = DFS(counter + 1)
+
+        if r1 is None or r2 is None:
+            return None
+        
+        if r1[1] < r2[0]:
+            return [r1[0], r2[1]]
+        elif r2[1] < r1[0]:
+            ops += 1
+            return [r2[0], r1[1]]
+        else:
+            ops = -1
+            return None
+        
+    res = DFS()
+    return -1 if res is None else ops
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
         n = int(log2(int(input())))
         p = list(map(int, input().split()))
-        index = -1
-        ops = 0
-
-        def DFS(counter: int = 0) -> Optional[List[int]]:
-            global n, p, index, ops
-
-            if counter == n:
-                index += 1
-                return [p[index], p[index]]
-            
-            r1 = DFS(counter + 1)
-            r2 = DFS(counter + 1)
-            if r1 is None or r2 is None:
-                return None
-            
-            if r1[0] < r2[0] and r1[1] < r2[0]:
-                return [r1[0], r2[1]]
-            elif r1[0] > r2[0] and r1[0] > r2[1]:
-                ops += 1
-                return [r2[0], r1[1]]
-            else:
-                ops = -1
-                return None
-            
-        DFS()
-        print(ops)
+        print(solve(n, p))
